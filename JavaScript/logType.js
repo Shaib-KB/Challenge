dd9ab08c5e84674a4f72c83d0a30d6d048a11952
kwanele <kwanelebuthelezi2001@gmail.com>
2024-08-10 22:27:33 +0200

    New

---
+++ JavaScript/logType.js
@@ -0,0 +1,28 @@
+// Create a file named logType.js.
+// Declare a variable named data.
+// Write a switch statement that takes the typeof data and logs a different string depending on the type/value of data.
+// The string it logs should be one of the following:
+
+// "undefined!"
+// "null!"
+// "number!"
+// "not a number!"
+// "string!"
+// "boolean!"
+// "bigint!"
+// "function!"
+// "object!"
+// "array!"
+// "I have no idea!"
+
+let data;
+
+switch (typeof data) {
+  case "undefined":
+    console.log("undefinde");
+
+    break;
+
+  default:
+    break;
+}
