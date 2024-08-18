dea571c84dfc05161e5864f316cbb36747000e08
kwanele <kwanelebuthelezi2001@gmail.com>
2024-08-18 20:10:03 +0200

    NEW

---
+++ JavaScript/js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
@@ -0,0 +1,9 @@
+console.clear();
+
+function handleUserLogin(onSuccess) {
+  // You might wonder why this function is called `onSuccess`. This will become clear by the fourth challenge! 🤞
+  onSuccess();
+}
+
+// Call handleUserLogin below!
+handleUserLogin;
---
+++ JavaScript/js-callback-functions_user-login-callback-3-with-multiple-arguments-1/js/index.js
@@ -0,0 +1,14 @@
+console.clear();
+
+// function handleUserLogin(onSuccess) {
+//   onSuccess("Jane Doe", "admin");
+// }
+
+// The exercise starts here!
+const fruits = ["Apple", "banana", "mango", "orange", "egg"];
+
+results = fruits.forEach(eachfruit);
+
+function eachfruit(Element) {
+  console.log(Element);
+}
